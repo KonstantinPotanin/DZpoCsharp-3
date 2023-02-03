5f854802324863e1392b603b03c2f4af1c4d577e
Konstantin <pkv90@yandex.ru>
2023-02-03 15:49:20 +0500

    Решение третьей задачи

---
+++ Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int GetNumber(string message)
+{
+    int result = 0;
+    
+    while (true)
+    {
+        Console.WriteLine(message);
+    
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else 
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+    return result;
+}
+void  GetCubeNum (int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)}, ");
+    }
+}
+int n = GetNumber("Введите число N");
+GetCubeNum (n);
